099288bb86630a7d98bef28aea2d394bcbe838d9
NAVEENMN <navimn1991@gmail.com>
2024-06-28 12:51:55 -0700

    Adding player

--- player.py
+++ player.py
@@ -4,18 +4,19 @@ import pygame
 class Player(object):
     def __init__(self):
         self.rect = pygame.rect.Rect((64, 54, 16, 16))
+        self.speed = 2
 
     def handle_keys(self):
         key = pygame.key.get_pressed()
         dist = 1
         if key[pygame.K_LEFT]:
-            self.rect.move_ip(-1, 0)
+            self.rect.move_ip(-1*self.speed, 0)
         if key[pygame.K_RIGHT]:
-            self.rect.move_ip(1, 0)
+            self.rect.move_ip(1*self.speed, 0)
         if key[pygame.K_UP]:
-            self.rect.move_ip(0, -1)
+            self.rect.move_ip(0, -1*self.speed)
         if key[pygame.K_DOWN]:
-            self.rect.move_ip(0, 1)
+            self.rect.move_ip(0, 1*self.speed)
 
     def draw(self, surface):
         pygame.draw.rect(surface, (0, 0, 128), self.rect)
